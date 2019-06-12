1e71891ba2e96385dab995440bb89429db9a6117
Ki deuk Park <zxcv4778@gmail.com>
2019-06-12 16:39:07 +0900

    update setting.py

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'k=9@1k4w0#stlblfkuvx^^h09x(c3d#8a-zw&88t1o#*adku$@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
